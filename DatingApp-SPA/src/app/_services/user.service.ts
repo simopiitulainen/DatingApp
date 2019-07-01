c7810dbde5891daaa2d87a6c8393f0a28d49e4fb
Simo Piitulainen <sipesapi@student.jyu.fi>
2019-07-01 17:23:48 +0300

    End of section 16

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { PaginatedResult } from '../_models/pagination';
 import { MapOperator, map } from 'rxjs/internal/operators/map';
+import { Message } from '../_models/message';
 
 
 
@@ -17,30 +18,38 @@ export class UserService {
 
 constructor(private http: HttpClient) {}
 
-getUsers(page?, itemsPerPage?, userParams?): Observable<PaginatedResult<User[]>> {
+getUsers(page?, itemsPerPage?, userParams?, likesParam?): Observable<PaginatedResult<User[]>> {
   const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<User[]>();
 
   let params = new HttpParams();
 
-  if(page != null && itemsPerPage != null) {
+  if (page != null && itemsPerPage != null) {
     params = params.append('pageNumber', page);
     params = params.append('pageSize', itemsPerPage);
   }
 
-  if(userParams != null)
-  {
+  if (userParams != null) {
     params = params.append('minAge', userParams.minAge);
     params = params.append('maxAge', userParams.maxAge);
     params = params.append('gender', userParams.gender);
     params = params.append('orderBy', userParams.orderBy);
   }
 
+  if (likesParam === 'Likers') {
+    params = params.append('likers', 'true');
+  }
+
+
+  if (likesParam === 'Likees') {
+    params = params.append('likees', 'true');
+  }
+
   return this.http.get<User[]>(this.baseUrl + 'users', {observe: 'response', params})
   .pipe(
     map(response => {
       paginatedResult.result = response.body;
-      if(response.headers.get('Pagination') != null ){
-        paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'))
+      if (response.headers.get('Pagination') != null ) {
+        paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'));
         return paginatedResult;
       }
     })
@@ -62,10 +71,59 @@ setMainPhoto(userId: number, id: number) {
 
 }
 
-deletePhoto(userId: number, id: number)
-{
+deletePhoto(userId: number, id: number) {
   return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + id);
 }
+
+sendLike(id: number, recipientId: number) {
+  return this.http.post(this.baseUrl + 'users/' + id + '/like/' + recipientId, {});
+}
+
+getMessages(id: number, page?, itemsPerPage?, messageCountainer?) {
+  const paginatedResult: PaginatedResult<Message[]> = new PaginatedResult<Message[]>();
+
+  let params = new HttpParams();
+
+  params = params.append('MessageContainer', messageCountainer);
+
+  if (page != null && itemsPerPage != null) {
+    params = params.append('pageNumber', page);
+    params = params.append('pageSize', itemsPerPage);
+  }
+
+  return this.http.get<Message[]>(this.baseUrl + 'users/' + id + '/messages', { observe: 'response', params})
+  .pipe(
+    map( response => {
+      paginatedResult.result = response.body;
+      if (response.headers.get('Pagination') !== null) {
+        paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'));
+      }
+
+      return paginatedResult;
+    })
+  );
+
+}
+
+getMessageThread(id: number, recipientId: number) {
+  return this.http.get<Message[]>(this.baseUrl + 'users/' + id + '/messages/thread/' + recipientId);
+}
+
+sendMessage(id: number, message: Message) {
+  return this.http.post(this.baseUrl + 'users/' + id + '/messages', message);
+}
+
+deleteMessage(id: number, userId: number) {
+  return this.http.post(this.baseUrl + 'users/' + userId + '/messages/' + id, {});
+
+}
+
+markAsRead(userId: number, messageId: number) {
+  this.http.post(this.baseUrl + 'users/' + userId + '/messages/' + messageId + '/read', {})
+  .subscribe();
+}
+
+
 
 }
 
